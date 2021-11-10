ef849d2d4a5690dc43107afba22c5927bceda601
Rafaela Brandão Mania Lima <60527114+Rafaela-create013@users.noreply.github.com>
2021-11-10 15:38:53 -0300

    Update pesquisa.php

--- pesquisa.php
+++ pesquisa.php
@@ -7,8 +7,8 @@
 	$msg .="	<thead>";
 	$msg .="		<tr>";
 	$msg .="			<th>Atendimento</th>";
-	$msg .="			<th>Nome</th>";
-	$msg .="			<th>Idade</th>";
+	$msg .="			<th>Setor</th>";
+	$msg .="			<th>Leito</th>";
 	$msg .="		</tr>";
 	$msg .="	</thead>";
 	$msg .="	<tbody>";
@@ -17,27 +17,30 @@
 				require_once('class/Conexao.class.php');
 					try {
 						$pdo = new Conexao(); 
-						$resultado = $pdo->select("SELECT * FROM tb_paciente 
+						$resultado = $pdo->select("SELECT * FROM tb_setor 
 													WHERE id_paciente LIKE '$parametro%' 
-													OR nm_paciente LIKE '$parametro%' 
-													OR idade_paciente LIKE '$parametro%' 
-													ORDER BY nm_paciente ASC");
+													OR nm_leito LIKE '$parametro%' 
+													OR nm_setor LIKE '$parametro%' 
+													ORDER BY nm_leito ASC");
 						$pdo->desconectar();
 								
 						}catch (PDOException $e){
 							echo $e->getMessage();
 						}	
+						global $i;
 						//resgata os dados na tabela
 						if(count($resultado)){
 							foreach ($resultado as $res) {
-
 	$msg .="				<tr>";
 	$msg .="					<td>".$res['id_paciente']."</td>";
-	$msg .="					<td>".$res['nm_paciente']."</td>";
-	$msg .="					<td>".$res['idade_paciente']."</td>";
+	$msg .="					<td>".$res['nm_setor']."</td>";
+	$msg .="					<td>".$res['nm_leito']."</td>";
+	$msg .="				<td><button>Chamar paciente</button><td>";
 	$msg .="				</tr>";
-							}	
-						}else{
+							
+							}
+						}
+						else{
 							$msg = "";
 							$msg .="Nenhum resultado foi encontrado...";
 						}

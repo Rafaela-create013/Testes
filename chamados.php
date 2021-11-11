2a2c0023ff13e4abf9120681e2275deb3335be2f
Rafaela Brandão Mania Lima <60527114+Rafaela-create013@users.noreply.github.com>
2021-11-11 16:57:50 -0300

    Update chamados.php

--- chamados.php
+++ chamados.php
@@ -5,8 +5,26 @@
     require_once('class/Conexao.class.php');
         try{
             $pdo = new Conexao();
-            $result = $pdo->select("SELECT ")
+            $result = $pdo->select("SELECT * FROM vw_atendimento 
+                                    WHERE id_paciente = '$id'");
+            $pdo->desconectar();
         }
-    
-
+        catch (PDOException $a){
+            echo $a->getMessage();
+        }
+        //concatenando e mostrando
+        if(count($result)){
+            foreach($result as $dados){
+$tela .="				<tr>";
+$tela .="					<td>".$dados['id_paciente']."</td>";
+$tela .="					<td>".$dados['nm_paciente']."</td>";
+$tela .="					<td>".$dados['nm_setor']."</td>";
+$tela .="					<td>".$dados['nm_leito']."</td>";
+$tela .="				</tr>";
+            }
+        }
+        else{
+            $msg .="Parece que ocorreu um erro...";
+        }
+    echo $tela;
 ?>

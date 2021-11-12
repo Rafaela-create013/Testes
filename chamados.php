ef532bbe4ed4fe5ac6a9001b5bb815aaabc0e9f0
Rafaela Brandão Mania Lima <60527114+Rafaela-create013@users.noreply.github.com>
2021-11-12 16:29:23 -0300

    Update chamados.php

--- chamados.php
+++ chamados.php
@@ -1,3 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Painel de pacientes</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/styleI.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+</head>
+<body>
+        <ul>
+            <p>Painel de pacientes</p>
+        </ul>
 <?php
     $id = isset($_GET['id']) ? $_GET['id'] : null;
     $tela = "";
@@ -5,7 +22,7 @@
     require_once('class/Conexao.class.php');
         try{
             $pdo = new Conexao();
-            $result = $pdo->select("SELECT * FROM vw_atendimento 
+            $result = $pdo->select("SELECT * FROM tb_paciente 
                                     WHERE id_paciente = '$id'");
             $pdo->desconectar();
         }
@@ -13,18 +30,32 @@
             echo $a->getMessage();
         }
         //concatenando e mostrando
+$tela .="<table class='table table-hover'>";
+$tela .="	<thead>";
+$tela .="		<tr>";
+$tela .="			<th>Atendimento</th>";
+$tela .="			<th>Paciente</th>";
+$tela .="			<th>Idade</th>";
+$tela .="		</tr>";
+$tela .="	</thead>";
+$tela .="	<tbody>";
+
         if(count($result)){
             foreach($result as $dados){
 $tela .="				<tr>";
-$tela .="					<td>".$dados['id_paciente']."</td>";
-$tela .="					<td>".$dados['nm_paciente']."</td>";
-$tela .="					<td>".$dados['nm_setor']."</td>";
-$tela .="					<td>".$dados['nm_leito']."</td>";
+$tela .="					<td id='tds'>".$dados['id_paciente']."</td>";
+$tela .="					<td id='tds'>".$dados['nm_paciente']."</td>";
+$tela .="					<td id='tds'>".$dados['idade_paciente']." anos </td>";
 $tela .="				</tr>";
             }
+
         }
         else{
-            $msg .="Parece que ocorreu um erro...";
+            $tela .="Parece que ocorreu um erro...";
         }
     echo $tela;
 ?>
+<textarea></textarea>
+
+
+
